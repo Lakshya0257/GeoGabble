8745f7cc037da0e1ee8963a64f5fd82dc9f43cb1
Lakshya0257 <lakshyabhati24@gmail.com>
2024-02-14 12:45:20 +0530

    shuttle deploy

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 mod connection;
 mod routes;
+use axum::Router;
 use routes::create_routes;
 mod core;
 mod models;
 mod utils;
 mod error;
 
-pub async fn run() {
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+pub async fn run() -> Router {
+    // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     let app = create_routes().await;
-    axum::serve(listener, app).await.unwrap();
+    app
+    // axum::serve(listener, app).await.unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
 use global_ms::run;
 mod connection;
 
-#[tokio::main]
-async fn main() {
-    // tracing_subscriber::fmt()
-    //     .with_max_level(tracing::Level::DEBUG)
-    //     .with_test_writer()
-    //     .init();
-    // let r_client = connect_redis();
-    // match r_client {
-    //     Ok(mut con) => {
-    //         println!("Connected to redis server");
-    //         let val: Result<HashMap<String,String>, redis::RedisError> = con.hgetall(format!("users:12356"));
-    //         println!("{:?}",val);
-    //         // let mut redis_client_mutex: Arc<Connection> = Arc::new(con);
-    //         run(con).await;
-    //     },
-    //     Err(err)=> {
-    //         println!("{:?}", err);
-    //     }
-    // }
-    run().await;
+#[shuttle_runtime::main]
+pub async fn axum (
+) -> shuttle_axum::ShuttleAxum {
+    // sqlx::migrate!()
+    //     .run(&pool)
+    //     .await
+    //     .expect("Migrations failed :(");
+
+    // Use secrets for anything that needs them
+
+    let router = run().await;
+
+    Ok(router.into())
 }
+
+// #[tokio::main]
+// async fn main() {
+//     // tracing_subscriber::fmt()
+//     //     .with_max_level(tracing::Level::DEBUG)
+//     //     .with_test_writer()
+//     //     .init();
+//     // let r_client = connect_redis();
+//     // match r_client {
+//     //     Ok(mut con) => {
+//     //         println!("Connected to redis server");
+//     //         let val: Result<HashMap<String,String>, redis::RedisError> = con.hgetall(format!("users:12356"));
+//     //         println!("{:?}",val);
+//     //         // let mut redis_client_mutex: Arc<Connection> = Arc::new(con);
+//     //         run(con).await;
+//     //     },
+//     //     Err(err)=> {
+//     //         println!("{:?}", err);
+//     //     }
+//     // }
+//     run().await;
+// }
